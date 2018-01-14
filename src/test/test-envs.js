f12c3f4345bc764197571bf8601a43bb8a52b09d
Matheus Fidelis <msfidelis@outlook.com>
2018-01-14 16:34:15 -0200

    mocha opts

---
+++ src/test/test-envs.js
@@ -0,0 +1,7 @@
+process.env.ENV = 'test';
+process.env.MYSQL_MASTER_SERVER = '0.0.0.0';
+process.env.MYSQL_MASTER_USER = 'root';
+process.env.MYSQL_MASTER_PASS = 'root';
+process.env.MYSQL_MASTER_SCHEMA = 'test';
+process.env.MYSQL_MASTER_PORT = '3306';
+process.env.SERVER_PORT = '1335';
